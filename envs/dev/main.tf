a95562e53ee45ffb20f7223971f9206e6b6345ee
srpaul2091 <shadana.paul@gmail.com>
2026-09-24 09:56:00 +0930

    Fix RDS master username

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -37,7 +37,7 @@ module "rds" {
 
   project                    = local.project
   env                        = local.env
-  username                   = "admin"
+  username                   = "pharmaadmin"
   password                   = var.db_password
   vpc_id                     = module.vpc.vpc_id
   db_subnet_group_name       = module.vpc.database_subnet_group_name
